12bbedb44af0bf811771286d3435f24dbcb1bcd0
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2025-08-03 12:15:52 -0400

    adjust navbar

--- src/lib/components/nav-bar.svelte
+++ src/lib/components/nav-bar.svelte
@@ -1,33 +1,36 @@
 <script lang="ts">
+	import MaterialSymbolsLightMode from '~icons/material-symbols/light-mode';
 	import { getIcon } from '$lib/generated/icons';
 	import type { SiteConfig } from '$lib/types';
 	let { data }: { data: SiteConfig } = $props();
 </script>
 
 <div
-	class="absolute left-0 right-0 top-0 z-10 flex flex-row border-b-1 p-2 border-gray-300 bg-white h-15
+	class="absolute top-0 right-0 left-0 z-10 flex h-15 flex-row border-b-1 border-gray-300 bg-white p-2
 	[&_a]:flex [&_a]:flex-row [&_a]:items-center [&_a]:gap-2 [&_a]:rounded
-	[&_a]:p-2 [&_a]:transition-all [&_a]:hover:bg-gray-200"
+	[&_a]:p-2 [&_a]:transition-all [&_a]:hover:bg-gray-200 [&_p]:hidden sm:[&_p]:flex"
 >
-	<a href="/">
-		{#if data.icon}
-			{@const IconComponent = getIcon(data.icon)}
-			<IconComponent />
-		{/if}
-		{data.name}
-	</a>
 	<nav class="flex flex-grow justify-evenly">
+		<a href="/">
+			{#if data.icon}
+				{@const IconComponent = getIcon(data.icon)}
+				<IconComponent />
+			{/if}
+			<p>{data.name}</p>
+		</a>
 		{#each Object.entries(data.navigation) as [key, navItem]}
 			{#if navItem.enabled}
 				<a href={`/${key}`}>
 					{#if navItem.icon}
 						{@const IconComponent = getIcon(navItem.icon)}
-						<IconComponent />
+						<div>
+							<IconComponent />
+						</div>
 					{/if}
-					{navItem.name}
+					<p>{navItem.name}</p>
 				</a>
 			{/if}
 		{/each}
 	</nav>
-	<button>Toggle</button>
+	<!-- <MaterialSymbolsLightMode/> -->
 </div>
