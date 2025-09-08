268b5148cf4f4d14eec9ab9e76d361decdb93b6d
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2025-09-08 11:15:59 -0400

    preserve line breaks

--- src/lib/components/project-cell.svelte
+++ src/lib/components/project-cell.svelte
@@ -7,7 +7,7 @@
 {#if type == 'title'}
 	<div class="m-4 flex flex-1 items-center justify-center text-3xl font-bold">{content}</div>
 {:else if type == 'text'}
-	<div class="m-4 overflow-y-auto">{content}</div>
+	<div class="m-4 overflow-y-auto whitespace-pre-line">{content}</div>
 {:else if type == 'image'}
 	<div class="p-4 mx-auto flex max-h-full justify-center">
 		<img src={content} alt={content} class="object-contain" />
